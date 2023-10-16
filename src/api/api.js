a2d18973e1eab80891219b1fa483a0746ef0f479
Cielo <houng1004@gmail.com>
2023-10-16 19:36:16 +0800

    fix: Add prefix to api url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: `${process.env.API_URL || 'http://localhost:5200'}/api`
+  baseURL: `${process.env.REACT_APP_API_URL || 'http://localhost:5200'}/api`
 })
 
 const EXCLUDED_URLS = ['/auth/login', '/auth/register'];
